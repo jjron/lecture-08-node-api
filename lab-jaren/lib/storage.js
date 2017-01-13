c3d723c5f609f6017d2211899a54372a55c032f1
jjron <jaren.escueta@gmail.com>
2017-01-12 16:43:15 -0800

    Removed comments

--- lab-jaren/lib/storage.js
+++ lab-jaren/lib/storage.js
@@ -3,13 +3,11 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 
-//const data = {};
 const storage = module.exports = {};
 
 const dataDir = `${__dirname}/../data`;
 
 storage.createItem = function(name, item) {
-  // TODONE: check if directory exists
   if (!(`${dataDir}/${name}`)) {
     fs.mkdirAsync(`${dataDir}/${name}`);
   }
@@ -20,23 +18,12 @@ storage.createItem = function(name, item) {
   })
   .then(() => {
     let json = JSON.stringify(item);
-    // TODONE: save the item in the directory
     return fs.writeFileAsync(`${dataDir}/${name}/${item.id}.json`, json);
   })
-  // TODONE: respond to the user
   .then(() => Promise.resolve(item));
 };
 
 storage.readItem = function(name, id) {
-  // if (!data[name] || !data[name][id]) {
-  //   let err = new Error('item not found');
-  //   err.status = 404;
-  //   return Promise.reject(err);
-  // }
-  // return Promise.resolve(data[name][id]);
-  // TODONE: check if name dir exists if not 404
-  // TODONE: check if JSON file with id name exists if not 404
-  // TODONE: read json file parse and send back
   if (!(`${dataDir}/${name}`) || !(`${dataDir}/${name}/${id}`)) {
     let err = new Error('item not found');
     err.status = 404;
@@ -56,15 +43,6 @@ storage.readItem = function(name, id) {
 };
 
 storage.deleteItem = function(name, id) {
-  // if (!data[name] || !data[name][id]) {
-  //   let err = new Error('item not found');
-  //   err.status = 404;
-  //   return Promise.reject(err);
-  // }
-  // delete data[name][id];
-  // return Promise.resolve();
-  // TODO: use fs.statAsync to check if file exists
-  // if not in catch block make the error a 404 and reject it
   return fs.statAsync(`${dataDir}/${name}/${id}.json`)
   .catch(err => {
     err.status = 404;
